6374b93be8e015239acd06b0975dcebe0337bc85
Eelis O <eelis.orhala@live.fi>
2024-04-13 16:19:40 +0300

    muutokset

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # ohtuvarasto
+<<<<<<< HEAD
 
 [![CI](https://github.com/EelisO1/ohtuvarasto/workflows/CI/badge.svg)](https://github.com/EelisO1/ohtuvarasto/actions)
+=======
+>>>>>>> 3c24b7f (muutokset)
--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -36,5 +36,11 @@ class TestVarasto(unittest.TestCase):
 
         self.varasto.ota_varastosta(2)
 
+        self.varasto.ota_varastosta(2)
+
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
-        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 6)
+
+        self.varasto.lisaa_varastoon(8)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)

e0af334af273080e27170a8867744059aa60a9c9
Eelis O <eelis.orhala@live.fi>
2024-04-18 19:59:32 +0300

    1 tehtävä

--- src/varasto.py
+++ src/varasto.py
@@ -1,5 +1,5 @@
 class Varasto:
-    def __init__(self, tilavuus, alku_saldo = 0):
+    def __init__(self, tilavuus, alku_saldo=0):
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
         else:
@@ -40,6 +40,7 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-        #kommentti väliaikainen
+        # kommentti väliaikainen
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
